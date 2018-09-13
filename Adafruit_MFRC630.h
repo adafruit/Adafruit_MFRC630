ad3a6ee07811c0fca5a699b86d2e30912b8efea9
Kevin Townsend <contact@microbuilder.eu>
2018-09-13 19:30:45 +0200

    Added doxygen comments

--- Adafruit_MFRC630.h
+++ Adafruit_MFRC630.h
@@ -75,43 +75,198 @@
 class Adafruit_MFRC630
 {
   public:
+    /**
+     * Constructor
+     *
+     * @param pdown_pin     The power down pin number (required)/
+     * @param i2c_addr      The I2C address to use (default value is empty)
+     */
     Adafruit_MFRC630(int8_t pdown_pin = -1, uint8_t i2c_addr = MFRC630_I2C_ADDR);
+
+    /**
+     * Constructor
+     *
+     * @param wireBus       The I2C bus to use
+     * @param pdown_pin     The power down pin number (required)/
+     * @param i2c_addr      The I2C address to use (default value is empty)
+     */
     Adafruit_MFRC630(TwoWire* wireBus, int8_t pdown_pin = -1, uint8_t i2c_addr = MFRC630_I2C_ADDR);
 
+    /**
+     * Initialises the IC and performs some simple system checks.
+     *
+     * @return True if init succeeded, otherwise false.
+     */
     bool begin(void);
 
     /* FIFO helpers (see section 7.5) */
+    /**
+     * Returns the number of bytes current in the FIFO buffer.
+     *
+     * @return The number of bytes in the FIFO buffer.
+     */
     int16_t readFIFOLen(void);
+
+    /**
+     * Reads data from the FIFO buffer.
+     *
+     * @param len       The number of bytes to read
+     * @param buffer    The buffer to write data into.
+     *
+     * @return The actual number of bytes read from the FIFO buffer.
+     */
     int16_t readFIFO(uint16_t len, uint8_t *buffer);
+
+    /**
+     * Write sdata into the FIFO buffer.
+     *
+     * @param len       The number of bytes to write.
+     * @param buffer    The data to write into the FIFO buffer.
+     *
+     * @return The actual number of bytes written.
+     */
     int16_t writeFIFO(uint16_t len, uint8_t *buffer);
+
+    /**
+     * Clears the contents of the FIFO buffer.
+     */
     void    clearFIFO(void);
 
     /* Command wrappers */
+    /**
+     * Sends an unparameterized command to the IC.
+     *
+     * @param command   The command register to send.
+     */
     void writeCommand(byte command);
+
+    /**
+     * Sends a parametrized command to the IC.
+     *
+     * @param command   The command register to send.
+     * @param paramlen  The number of parameter bytes.
+     * @param params    The paramater values to send.
+     */
     void writeCommand(byte command, uint8_t paramlen, uint8_t *params);
 
     /* Radio config. */
+    /**
+     * Configures the radio for the specified protocol.
+     *
+     * @param cfg   The radio config setup to use.
+     *
+     * @return True if succeeded, otherwise false.
+     */
     bool configRadio(mfrc630radiocfg cfg);
 
     /* General helpers */
+    /**
+     * Returns the current 'comm status' of the IC's internal state machine.
+     *
+     * @return The 8-bit state ID.
+     */
     uint8_t getComStatus(void);
+
+    /**
+     * Performs a soft-reset to put the IC into a known state.
+     */
     void    softReset(void);
 
     /* Generic ISO14443a commands (common to any supported card variety). */
+    /**
+     * Sends the REQA command, requesting an ISO14443A-106 tag.
+     *
+     * @return The ATQA value if a card was detected.
+     */
     uint16_t iso14443aRequest(void);
+
+    /**
+     * Sends the WUPA wakeup command.
+     *
+     * @return The ATQA value if a card was detected.
+     */
     uint16_t iso14443aWakeup(void);
+
+    /**
+     * Selects a detected ISO14443A card, retrieving the UID and SAK.
+     *
+     * @param uid   Pointer to the buffer where the uid should be written.
+     * @param sak   Pointer to the placeholder for the SAK value.
+     *
+     * @return True if init succeeded, otherwise false.
+     */
     uint8_t  iso14443aSelect(uint8_t *uid, uint8_t *sak);
 
     /* Mifare commands. */
+    /**
+     * Loads the specified authentication keys on the IC.
+     *
+     * @param key   Pointer to the buffer containing the key values.
+     */
     void     mifareLoadKey(uint8_t *key);
+
+    /**
+     * Authenticates the selected card using the previously supplied key/
+     *
+     * @param key_type  Whether to use KEYA or KEYB for authentication.
+     * @param blocknum  The block number to authenticate.
+     * @param uid       The UID of the card to authenticate.
+     *
+     * @return True if init succeeded, otherwise false.
+     */
     bool     mifareAuth(uint8_t key_type, uint8_t blocknum, uint8_t *uid);
+
+    /**
+     * Reads the contents of the specified (and previously authenticated)
+     * memory block.
+     *
+     * @param blocknum  The block number to read.
+     * @param buf       The buffer the data should be written into.
+     *
+     * @return The number of bytes read.
+     */
     uint16_t mifareReadBlock(uint8_t blocknum, uint8_t *buf);
+
+    /**
+     * Writes the supplied data to the previously authenticated
+     * memory block.
+     *
+     * @param blocknum  The block number to read.
+     * @param buf       The buffer holding the data to write.
+     *
+     * @return The number of bytes written.
+     */
     uint16_t mifareWriteBlock(uint16_t blocknum, uint8_t *buf);
+
+    /**
+     * The default key for fresh Mifare cards.
+     */
     uint8_t  mifareKeyGlobal[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+    /**
+     * The default key for NDEF formatted cards.
+     */
     uint8_t  mifareKeyNDEF[6]   = { 0xD3, 0xF7, 0xD3, 0xF7, 0xD3, 0xF7 };
 
     /* NTAG commands */
+    /**
+     * Reads the contents of the specified page.
+     *
+     * @param pagenum   The page number to read.
+     * @param buf       The buffer the data should be written into.
+     *
+     * @return The number of bytes read.
+     */
     uint16_t ntagReadPage(uint16_t pagenum, uint8_t *buf);
+
+    /**
+     * Writes the supplied content of the specified page.
+     *
+     * @param pagenum   The page number to write to.
+     * @param buf       The data to write to the card.
+     *
+     * @return The number of bytes written.
+     */
     uint16_t ntagWritePage(uint16_t pagenum, uint8_t *buf);
 
   private:
