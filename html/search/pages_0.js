ed126a556b7c442b3722b26b892a42829257bc6e
Doxygen CI <travis@travis-ci.org>
2020-06-10 18:56:41 +0000

    Deploy code docs to GitHub Pages Travis build: 9
    Commit: 7586b00b26922a29e2ad51b678d4a38f80d36c8c

--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['adafruit_20mfrc630_20rfid_20front_2dend_20driver_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmfrc630_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmfrc630_29',['Adafruit MFRC630 RFID Front-End Driver [![Build Status](https://travis-ci.org/adafruit/Adafruit_MFRC630.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MFRC630)',['../md__r_e_a_d_m_e.html',1,'']]]
-];
